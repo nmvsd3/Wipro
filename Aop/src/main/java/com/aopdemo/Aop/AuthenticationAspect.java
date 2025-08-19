5c86b48de807b291a87ae22e4d7f5d85ab26b930
Madhu <nedunurimadhu93@gmail.com>
2025-08-19 17:13:57 +0530

    cls work and activity

---
+++ Aop/src/main/java/com/aopdemo/Aop/AuthenticationAspect.java
@@ -0,0 +1,26 @@
+package com.aopdemo.Aop;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class AuthenticationAspect {
+
+	@Pointcut("within(com.aopdemo.Aop..*)")
+	public void authenticatingPointcut() {
+		
+	}
+	
+	@Pointcut("within(com.aopdemo.Aop.ShoppingCart.*)")
+	public void authorizationPointcut() {
+		
+	}
+	
+	@Before("authenticatingPointcut() && authorizationPointcut()")
+	public void authenticate() {
+		System.out.println("Authenticating request");
+	}
+}
---
+++ Aop/src/main/java/com/aopdemo/Aop/beanConfig.java
@@ -0,0 +1,12 @@
+package com.aopdemo.Aop;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@EnableAspectJAutoProxy
+@Configuration
+@ComponentScan(basePackages = "com.aopdemo.Aop")
+public class beanConfig {
+
+}
