42fb970e899e02516e25923ec6781a07a4d4f7ee
Madhu <nedunurimadhu93@gmail.com>
2025-08-27 18:03:41 +0530

    springboot

---
+++ SpringBoot_Wipro/SpringBootGlobalExceptionHandler/src/main/java/com/example/geh/dao/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.example.geh.dao;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.example.geh.model.Product;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
